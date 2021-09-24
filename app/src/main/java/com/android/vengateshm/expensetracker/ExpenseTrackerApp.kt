840d0fa1dba9bc0718e06b69261927fd1ac580c7
Vengatesh M <vengateshm.13@gmail.com>
2021-09-25 00:08:25 +0530

    prepopulate db using work manager

--- app/src/main/java/com/android/vengateshm/expensetracker/ExpenseTrackerApp.kt
+++ app/src/main/java/com/android/vengateshm/expensetracker/ExpenseTrackerApp.kt
@@ -1,8 +1,21 @@
 package com.android.vengateshm.expensetracker
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class ExpenseTrackerApp : Application() {
+class ExpenseTrackerApp : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        // Custom work manager initialization
+        return Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+    }
 }
